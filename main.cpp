d6dc18a678678652893fa40045bf6fcd710cbdab
Thomppa030 <kpriesnitz@outlook.de>
2024-03-24 19:54:50 +0100

    Implement logging and validation services
    This update adds logging and validation services to the main application. Various configurations for both services were initiated, and the services were registered using the ServiceRegistry. This enables better tracking and monitoring of application behavior.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 
-#include "ThryveApplication.h"
 #include "Core/App.h"
+#include "Core/Log.h"
+#include "Core/ServiceRegistry.h"
+#include "ThryveApplication.h"
 
 int main() {
+
+    Thryve::Core::DevelopmentLoggerConfiguration _devLogConfig = {};
+    _devLogConfig.ConsoleOutputEnabled = true;
+
+    Thryve::Core::ValidationLayerLoggerConfiguration _valLogConfig = {};
+    _valLogConfig.ConsoleOutputEnabled = true;
+
+    auto _loggingService = Thryve::Core::SharedRef<Thryve::Core::DevelopmentLogger>::Create("Core");
+    _loggingService->Init(&_devLogConfig);
+
+    auto _validationLoggerService = Thryve::Core::SharedRef<Thryve::Core::ValidationLayerLogger>::Create("Validation");
+    _validationLoggerService->Init(&_valLogConfig);
+
+    Thryve::Core::ServiceRegistry::RegisterService(_loggingService);
+    Thryve::Core::ServiceRegistry::RegisterService(_validationLoggerService);
 
     auto* _coreApp = new Thryve::Core::App();
 
     try {
         _coreApp->Run();
-
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
