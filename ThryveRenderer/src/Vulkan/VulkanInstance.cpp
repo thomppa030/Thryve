982838215f2ba7a3b975879a1d4ae41a6d95bb55
Kay Priesnitz <kpriesnitz@outlook.de>
2024-03-29 12:59:00 +0100

    Update application name in VulkanInstance
    The application's name in the VulkanInstance appInfo is updated to be more dynamic. Instead of the hardcoded Value, it now uses the applicationName variable, enhancing flexibility and maintainability within the code.

--- ThryveRenderer/src/Vulkan/VulkanInstance.cpp
+++ ThryveRenderer/src/Vulkan/VulkanInstance.cpp
@@ -93,7 +93,7 @@ namespace Thryve::Rendering {
 
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pApplicationName = applicationName.c_str();
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
